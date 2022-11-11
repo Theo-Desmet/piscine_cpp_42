717366fafb9549049300de920bdba3c9b647f8dd
Theo DESMET <tdesmet@e1r3p18.clusters.42paris.fr>
2022-11-11 11:16:47 +0100

    finish ex01

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -9,13 +9,15 @@ Fixed::Fixed() : _number(0) {
 	return ;
 }
 
-Fixed::Fixed( int const nb ) : _number(nb << Fixed::getWidth()){
+Fixed::Fixed( int const nb ) {
 	std::cout << "Int constructor called" << std::endl;
+	this->_number = nb << this->_width;
 	return ;
 }
 
-Fixed::Fixed( float const nb ) : _number(static_cast<int>(roundf(nb)) << Fixed::getWidth()) {
+Fixed::Fixed( float const nb ) {
 	std::cout << "Float constructor called" << std::endl;
+	this->_number = roundf(nb * float((1 << this->_width)));
 	return ;
 }
 
@@ -30,18 +32,17 @@ Fixed::~Fixed() {
 	return ;
 }
 
-Fixed & Fixed::operator=(Fixed const & rhs) {
+Fixed &	Fixed::operator=(Fixed const & rhs) {
 	std::cout << "Copy assignment operator called" << std::endl;
 	this->_number  = rhs.getRawBits();
 	return (*this);
 }
 
-int	Fixed::getRawBits() const{
-	std::cout << "getRawBits member function called" << std::endl;
+int		Fixed::getRawBits() const{
 	return (this->_number);
 }
 
-int	Fixed::getWidth() const{
+int		Fixed::getWidth() const{
 	return (this->_width);
 }
 
@@ -49,4 +50,17 @@ void	Fixed::setRawBits(int const raw) {
 	std::cout << "setRawBits member function called" << std::endl;
 	_number = raw;
 	return ;
+}
+
+int		Fixed::toInt() const{
+	return (this->_number >> Fixed::getWidth());
+}
+
+float		Fixed::toFloat() const{
+	return ((float)this->_number / (float)(1 << this->_width));
+}
+
+std::ostream & operator<<(std::ostream & out, Fixed const & rhs) {
+	out << rhs.toFloat();
+	return (out);
 }
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -1,25 +1,31 @@
 #ifndef FIXED_EX00_FIXED_H
 #define FIXED_EX00_FIXED_H
 
+#include <iostream>
+
 class Fixed {
-	
+
 	public:
 		Fixed( void );
 		Fixed( int const );
 		Fixed( float const );
 		Fixed( Fixed const & );
 		~Fixed( void );
+
 		Fixed & operator=( Fixed const & );
 
 		int	getRawBits( void ) const;
 		int	getWidth( void ) const;
-		void	setRawBits( int const raw );
+
+		int		toInt( void ) const;
 		float	toFloat( void ) const;
-		int	toInt( void ) const;
+		void	setRawBits( int const raw );
 
 	private:
 		int		_number;
 		int static const	_width = 8;
 };
+
+std::ostream & operator<<(std::ostream & out, Fixed const & rhs);
 
 #endif
