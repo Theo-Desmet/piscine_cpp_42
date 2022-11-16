83d92722779983cfbffc6b9e52fcf49998b4e7c0
Theo DESMET <tdesmet@e1r2p20.clusters.42paris.fr>
2022-11-16 17:02:45 +0100

    begin cpp03/ex01

---
+++ cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdesmet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 08:39:21 by tdesmet           #+#    #+#             */
+/*   Updated: 2022/11/16 16:26:00 by tdesmet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_CPP03_EX01_SCAVTRAP_H
+#define CLAPTRAP_CPP03_EX01_SCAVTRAP_H
+
+#define RED "\033[1;31mred\033[1;0m"
+#define GREEN "\033[1;32mgreen\033[1;0m"
+#define YELLOW "\033[1;33myellow\033[1;0m"
+#define BLUE "\033[1;34mblue\033[1;0m"
+#define MAGENTA "\033[1;35mmagenta\033[1;0m"
+#define CYAN "\033[1;36mcyan\033[1;0m"
+
+#include <iostream>
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+	public:
+		ScavTrap( void );
+		ScavTrap( std::string const name );
+		ScavTrap( ScavTrap const & cpy );
+
+		~ScavTrap( void );
+
+		ScavTrap & operator=( ScavTrap const & cpy );
+
+		void	attack( const std::string& target );
+		void	guardGate( void );
+};
+
+std::ostream & operator<<(std::ostream & out, ScavTrap const & hrs);
+
+#endif
