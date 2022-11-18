92e69779f3aa2811dbcd08668c4232b77339bab3
Theo DESMET <tdesmet@e1r3p18.clusters.42paris.fr>
2022-11-18 08:41:58 +0100

    normalize main in cpp03/ex00

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tdesmet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 10:27:54 by tdesmet           #+#    #+#             */
-/*   Updated: 2022/11/16 15:44:30 by tdesmet          ###   ########.fr       */
+/*   Updated: 2022/11/18 08:30:27 by tdesmet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,13 @@ int	main() {
 	std::cout << "************** init ***************" << std::endl;
 	ClapTrap	foo;
 	ClapTrap	red(RED);
-	ClapTrap	red2(red);
-	ClapTrap	blue(BLUE);
-	foo = blue;
+	ClapTrap	bar(red);
+	foo = bar;
 
 	std::cout << std::endl << "********** test attack ************" << std::endl;
 	red.attack("bar");
 	red.attack(BLUE);
-	blue.attack(RED);
-	blue.attack("");
-	blue.attack(BLUE);
+	red.attack("");
 
 	std::cout << std::endl << "********** test repar/damage ************" << std::endl;
 	for (int i = 0; i < 4; i++) {
@@ -43,13 +40,13 @@ int	main() {
 
 	std::cout << std::endl << "************ test energy **************" << std::endl;
 	for (int i = 0; i < 10; i++) {
-		std::cout << red2 << std::endl;
-		red2.attack("bar");
+		std::cout << bar << std::endl;
+		bar.attack("bar");
 	}
-	std::cout << red2 << std::endl;
-	red2.attack("bar");
-	std::cout << red2 << std::endl;
-	red2.beRepaired(0);
+	std::cout << bar << std::endl;
+	bar.attack("bar");
+	std::cout << bar << std::endl;
+	bar.beRepaired(0);
 
 	std::cout << std::endl << "********* call destructor **********" << std::endl;
 }
